d25a9b1088e1af869d5170f8de1e59adca3cb0d4
SorehaDAME <51748896+ag0609@users.noreply.github.com>
2021-06-17 10:09:57 +0800

    Create Templete.inc.js

---
+++ Templete.inc.js
@@ -0,0 +1,14 @@
+//Templete.inc.js
+//Description: Just a templete, edit as you like
+
+const $ver = "0.01a";
+console.log("Templete Engine ver." + $ver);
+
+//Init
+
+//Operations
+_tzr_start = function() {};
+_tzr_stop = function() {};
+_tzr_prev = function() {};
+_tzr_next = function() {};
+_tzr_jump = function() {};
