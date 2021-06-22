8d68874642818a0500b47dbf0898e0cfeb4c44c0
SorehaDAME <51748896+ag0609@users.noreply.github.com>
2021-06-22 18:33:47 +0800

    Create bookwalkerjp.inc.js

---
+++ Engine/BWJP/bookwalkerjp.inc.js
@@ -0,0 +1,25 @@
+//bookwalkerjp.inc.js
+//Description: Just a templete, edit as you like
+
+const $ver = "0.01a";
+console.log("Bookwalker Japan Engine ver." + $ver);
+
+//Init
+const __NFBR = NFBR;
+
+//Operations
+_tzr_start = function() {
+  
+};
+_tzr_stop = function() {
+
+};
+_tzr_prev = function() {
+
+};
+_tzr_next = function() {
+
+};
+_tzr_jump = function() {
+
+};
