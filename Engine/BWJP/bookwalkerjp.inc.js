ce15cebb80bab54847fb75476cb8735254e154b4
SorehaDAME <51748896+ag0609@users.noreply.github.com>
2023-03-09 10:41:51 +0800

    modified:   Engine/BWJP/bookwalkerjp.inc.js 	new file:   tzuzuri.meta.user.js

--- Engine/BWJP/bookwalkerjp.inc.js
+++ Engine/BWJP/bookwalkerjp.inc.js
@@ -5,17 +5,21 @@ const $ver = "0.01a";
 console.log("Bookwalker Japan Engine ver." + $ver);
 
 //Init
+//magic words, [root, core, utility, drawCanvas, downloadConfig, getCoord]
+let magicWords = ['a6G', 'F5W', 'a61', 'a5x', 'U8j', 'n0v'];
+let pace = 150, paceSeed = 100; //pace = job rest between pages, paceSeed = addition random time makes more like humans
+//cP=currentPage, tp=totalPage, 
 let cP=0, tP=0, rF=0, pF=0, dL=new Set(), zip=new JSZip(), coords={}, pages={};
 const path_arr = ["a6G", "Initializer", "F5W", "menu", "a6l"];
 if(!NFBR) NFBR = (unsafeWindow ? unsafeWindow.NFBR : window.NFBR) || null;
 if(!NFBR) {console.error("NFBR not located");} else if(!chkpath(_NFBR, path_arr)) {console.error("control not found");} else {rF=1};
-let menu = NFBR.a6G.Initializer.F5W.menu, control = menu.a6l;
+let menu = NFBR[magicWords[0]].Initializer[magicWords[1]].menu, control = menu[magicWords[2]];
 _tzr_next = control.moveToNext;
 _tzr_back = control.moveToPrevious;
 _tzr_jump = control.moveToPage;
-let tP=menu.model.get('F8O'),;
-let canvas=_NFBR.a6G.Initializer.F5W.renderer.currentScreen.canvas, context = canvas.getContext("2d");
-let plan = 1;
+tP=menu.model.get('F8O');
+let canvas=_NFBR[magicWords[0]].Initializer[magicWords[1]].renderer.currentScreen.canvas, context = canvas.getContext("2d");
+let plan = 1; //0=unclassified, 1=purchased, 2=trial
 
 try {
   if(window.location.hostname.match(/viewer-trial/i)) {
@@ -24,40 +28,39 @@ try {
 } catch(e) {
   plan = 0;
 }
-NFBR.a6G.Initializer.F5W.renderer.el.style.opacity = 0;
+canvas.style.opacity = 0;
 
 let _tzr_ui = document.createElement('div');
 
 step1();
-document.body.addEventListener("keyup", hideshow);
+document.body.addEventListener("keydown", keyEvent);
 //Operations
-_tzr_start = function() {
-  
-  if(!sF) {
-    sF = 1;
-    
-  } else {
-    
-    return;
+
+let _tzr = {
+  start: ()=>{
+    if(!sF) {
+
+    }
+  },
+  stop: ()=>{
+
   }
-};
-_tzr_stop = function() {
-  
 };
 
 //Extended Core
-let hideshow = function() {
+let keyEvent = function() {
   
 }
 let chkpath = function(root, path) {
   if(root && Array.isArray(path)) {
-    let cur = root;
+    let cur = root, cpath = [''];
     for(let i=0; i<path.length; i++) {
       if(typeof cur[path[i]] != "undefined") {
         cur = cur[path[i]];
+        cpath.push(path[i]);
         continue;
       } else {
-        console.warn("Undefined when checking", path[i]);
+        console.warn("Undefined when checking", cpath.join('/'));
         return false;
       }
     }
@@ -69,28 +72,29 @@ let chkpath = function(root, path) {
 }
 //hook
 let step1 = function() {
-  const backup = NFBR.a6G.a5x.prototype['U8j'];
-  NFBR.a6G.a5x.prototype['U8j'] = function() {
+  const Bp = NFBR[magicWords[0]][magicWords[3]].prototype[magicWords[4]];
+  NFBR[magicWords[0]][magicWords[3]].prototype[magicWords[4]] = function() {
     const [tC, p, i, dR, f] = arguments;
-    cP = 
+    cP = 0;
     if(p) {
       let name = p.url.match(/(?<=\/)[^/]+(?=\.xhtml)/)[0];
       let c = Object.keys(coords).length + 1;
       if(!coords[name]) {
         pages[name] = p;
-        coords[name] = NFBR.a6G.Initializer.F5W.renderer['n0v'](p, i.width, i.height);
+        coords[name] = NFBR[magicWords[0]].Initializer[magicWords[1]].renderer[magicWords[5]](p, i.width, i.height);
       }
-      let nextPace = 150 + Math.random()*100;
+      let nextPace = pace + Math.random()*paceSeed;
       setTimeout(_tzr_next, nextPace);
       if(cP >= tP) {
         sF=0;
         step2();
       }
     }
-    return backup.apply(this, arguments);
+    return Bp.apply(this, arguments);
   }
   
 }
+//afterMath
 let step2 = function() {
   
 }
---
+++ tzuzuri.meta.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Tsuzuri
+// @name:ja      綴-つづり-
+// @name:zh      綴-Tsuzuri-
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Don't use if you don't know what is this
+// @author       ag0609
+// @match        https://viewer.bookwalker.jp/*
+// @match        https://viewer-trial.bookwalker.jp/*
+// @match        https://viewer-ptrial.bookwalker.jp/*
+// @match        https://viewer-subscription.bookwalker.jp/*
+// @match        https://play.dlsite.com/*
+// @icon         https://www.favicon-generator.org/download/2015-01-26/ef8f0d893073e040807bbecfb35332c4.ico
+// @grant        unsafeWindow
+// @grant        GM.setValue
+// @grant        GM.getValue
+// @grant        GM_addStyle
+// @grant        GM.notification
+// @run-at       document-end
+// ==/UserScript==
